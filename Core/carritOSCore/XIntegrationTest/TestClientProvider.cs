bb134a485032b541def61e0df9000c62bb7992d4
DiegoAloslla <diegoalosilla@gmail.com>
2019-06-23 19:11:41 -0500

    :tada: IntegrationTestCRUD terminado

--- Core/carritOSCore/XIntegrationTest/TestClientProvider.cs
+++ Core/carritOSCore/XIntegrationTest/TestClientProvider.cs
@@ -5,19 +5,29 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
-
+/**
+* https://www.youtube.com/watch?v=p5l7x_pFjmI&t=40s
+* @author Juan Diego Alosilla
+* @email diegoalosillagmail.com
+*/
 namespace XIntegrationTest
 {
-    public class TestClientProvider
+  
+    public class TestClientProvider:IDisposable
     {
-
-        public HttpClient Client { get; private set; }
+        private TestServer server;
+        public HttpClient Client { get; set; }
 
         public TestClientProvider()
         {
-            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
             Client = server.CreateClient();
         }
 
+        public void Dispose()
+        {
+            server?.Dispose();
+            Client?.Dispose();
+        }
     }
 }
